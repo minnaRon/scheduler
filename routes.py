f5466e893703347ff028f6a5f51fa86cf9200e80
Minna Ronka <minna.ronka@gmail.com>
2021-05-02 22:39:03 +0300

    Create new group separately

--- routes.py
+++ routes.py
@@ -49,32 +49,44 @@ def register():
                         print("events reg", event)
                         users.add_event(event, session["user_id"], 2)
                     return redirect("/calendar")
-        
-        #tämä käytössä vain kerran, kun perustaja tallentaa ryhmän tiedot..
-        if request.form["group_reg"] == "new_group":
-            group_name = request.form["group_name"]
-            group_description = request.form["group_description"]
-            
-            if len(group_name) < 5 or len(group_name) > 20:
-                return render_template("error.html", message="Ryhmän nimen tulee sisältää 5-20 merkkiä")
-            if len(group_description) > 400:
-                return render_template("error.html", message="Ryhmän kuvauksen tulee sisältää enintään 400 merkkiä")
-            group_password1 = request.form["password_newgroup1"]
-            group_password2 = request.form["password_newgroup2"]
-            if group_password1 != group_password2:
-                return render_template("error.html", message="Liittymissalasanoissa oli eroa")
-            if group_password1 == "":
-                return render_template("error.html", message="Liittymissalasana oli tyhjä")
-            
-            group_id = group.add(group_name, group_description, group_password1)
-            if users.register(username, password1, 0):
-                group.set_founder(session["user_id"])
-                return redirect("/calendar")
-            else:
-                group.delete()
-                users.delete_user(username)
-        #...tähän asti uuden ryhmän perustaminen
+            return render_template("error.html", message="Rekisteröinti ei onnistunut, tarkista ryhmän liittymissalasana")
         return render_template("error.html", message="Rekisteröinti ei onnistunut")
+
+#tämä käytössä, kun ryhmä perustetaan
+@app.route("/register_new_group", methods=["GET","POST"])
+def register_new_group():
+    if request.form["group_reg"] == "new_group":
+        username = request.form["username"].strip()
+        if len(username) < 2 or len(username) > 35:
+            return render_template("error.html", message="Tunnuksen tulee sisältää 2-35 merkkiä")
+        password1 = request.form["password1"].strip()
+        password2 = request.form["password2"].strip()
+        if password1 != password2:
+            return render_template("error.html", message="Salasanoissa oli eroa")
+        if password1 == "":
+            return render_template("error.html", message="Salasana oli tyhjä")
+
+        group_name = request.form["group_name"]
+        group_description = request.form["group_description"] 
+        if len(group_name) < 5 or len(group_name) > 20:
+            return render_template("error.html", message="Ryhmän nimen tulee sisältää 5-20 merkkiä")
+        if len(group_description) > 400:
+            return render_template("error.html", message="Ryhmän kuvauksen tulee sisältää enintään 400 merkkiä")
+        group_password1 = request.form["password_newgroup1"]
+        group_password2 = request.form["password_newgroup2"]
+        if group_password1 != group_password2:
+            return render_template("error.html", message="Liittymissalasanoissa oli eroa")
+        if group_password1 == "":
+            return render_template("error.html", message="Liittymissalasana oli tyhjä")
+
+        group_id = group.add(group_name, group_description, group_password1)
+        if users.register(username, password1, 0):
+            group.set_founder(session["user_id"])
+            return redirect("/calendar")
+        else:
+            group.delete()
+            users.delete_user(username)
+    return render_template("error.html", message="Rekisteröinti ei onnistunut")
 
 @app.route("/logout")
 def logout():
